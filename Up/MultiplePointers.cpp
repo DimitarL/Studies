2ed0ecb369f42542f048c5dbe9ee7ab56ba04c2e
DimitarL <36930531+DimitarL@users.noreply.github.com>
2018-02-28 20:21:48 +0200

    Rename ConsoleApplication1.cpp to MultiplePointers.cpp

---
+++ Up/MultiplePointers.cpp
@@ -0,0 +1,27 @@
+﻿// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+void swap(int*, int*);
+
+int main()
+{
+	int c = 0;
+	int d = 5;
+	swap(&c, &d);
+
+	cout << c << "    " << d;
+
+    return 0;
+}
+
+void swap(int* a, int* b)
+{
+	*a =*a+ *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
