3a64c2e923c55ad9e609a566659b3e126b083bea
DimitarL <36930531+DimitarL@users.noreply.github.com>
2018-02-28 20:38:00 +0200

    Rename ConsoleApplication1.cpp to HeapSample.cpp

---
+++ Up/HeapSample.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int* harakiru();
+
+int main()
+{
+	int *p = harakiru();
+	cout << *p;
+	delete p;
+
+	return 0;
+}
+
+int* harakiru()
+{
+	int* p = new int;
+	*p = 30;
+	return p;
+}
